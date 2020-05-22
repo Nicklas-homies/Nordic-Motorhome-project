b01a0d8d8760f94b9d96f213065b962a8951c337
joni2good <57601655+joni2good@users.noreply.github.com>
2020-05-22 13:06:53 +0200

    Add files via upload

--- SQL stuff/Create DB and Tables.sql
+++ SQL stuff/Create DB and Tables.sql
@@ -1,27 +1,44 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema NMH_company
+-- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `NMH_company` ;
+
+-- -----------------------------------------------------
+-- Schema NMH_company
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `NMH_company` DEFAULT CHARACTER SET utf8 ;
 USE `NMH_company` ;
 
+-- -----------------------------------------------------
+-- Table `NMH_company`.`motorhomeType`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`motorhomeType` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`motorhomeType` (
   `typeId` INT NOT NULL,
   `capacity` INT NOT NULL,
-  `price` DOUBLE NOT NULL,
+  `price` INT NOT NULL,
   PRIMARY KEY (`typeId`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `NMH_company`.`motorhomes`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`motorhomes` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`motorhomes` (
   `motorhomeId` INT NOT NULL AUTO_INCREMENT,
   `brand` VARCHAR(45) NOT NULL,
   `model` VARCHAR(45) NOT NULL,
   `timesUsed` INT NULL,
   `kmDriven` INT NULL,
-  `extraPrice` DOUBLE NULL,
   `activeState` INT DEFAULT 0 NOT NULL,
   `typeId` INT NOT NULL,
   PRIMARY KEY (`motorhomeId`),
@@ -33,7 +50,12 @@ CREATE TABLE IF NOT EXISTS `NMH_company`.`motorhomes` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `NMH_company`.`customers`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`customers` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`customers` (
   `customerId` INT NOT NULL AUTO_INCREMENT,
   `cName` VARCHAR(70) NOT NULL,
@@ -41,11 +63,17 @@ CREATE TABLE IF NOT EXISTS `NMH_company`.`customers` (
   PRIMARY KEY (`customerId`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `NMH_company`.`custUseMotor`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`custUseMotor` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`custUseMotor` (
   `rentId` INT NOT NULL AUTO_INCREMENT,
   `startDate` DATE NOT NULL,
   `endDate` DATE NOT NULL,
+  `extraPrice` DOUBLE NULL,
   `customerId` INT NOT NULL,
   `motorhomeId` INT NOT NULL,
   PRIMARY KEY (`rentId`),
@@ -63,7 +91,12 @@ CREATE TABLE IF NOT EXISTS `NMH_company`.`custUseMotor` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `NMH_company`.`season`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`season` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`season` (
   `seasonId` INT NOT NULL,
   `startDate` DATE NOT NULL,
@@ -72,9 +105,14 @@ CREATE TABLE IF NOT EXISTS `NMH_company`.`season` (
   PRIMARY KEY (`seasonId`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `NMH_company`.`damages`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `NMH_company`.`damages` ;
+
 CREATE TABLE IF NOT EXISTS `NMH_company`.`damages` (
-  `damageId` INT NOT NULL AUTO_INCREMENT,
+  `damageId` INT NOT NULL,
   `damageDesc` VARCHAR(500) NULL,
   `motorhomeDmgId` INT NOT NULL,
   PRIMARY KEY (`damageId`),
@@ -86,7 +124,6 @@ CREATE TABLE IF NOT EXISTS `NMH_company`.`damages` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 INSERT INTO motorhometype (typeid, capacity, price) VALUES (1, 2, 400.00);
 INSERT INTO motorhometype (typeid, capacity, price) VALUES (2, 3, 600.00);
 INSERT INTO motorhometype (typeid, capacity, price) VALUES (3, 4, 800.00);
@@ -96,42 +133,43 @@ INSERT INTO motorhometype (typeid, capacity, price) VALUES (6, 8, 1600.00);
 INSERT INTO motorhometype (typeid, capacity, price) VALUES (7, 10, 2000.00);
 INSERT INTO motorhometype (typeid, capacity, price) VALUES (8, 12, 2400.00);
 
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGo',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGo',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGo',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGo',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Volvo','45Small',0,0,0,2);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Volvo','45Small',0,0,0,2);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Volvo','45Small',0,0,0,2);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Ford','Ka',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Ford','Ka',0,0,0,1);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Mercedes','VeryBig',0,0,0,3);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Mercedes','VeryBig',0,0,0,3);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Mercedes','VeryBig',0,0,0,3);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGoBig',0,0,0,4);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Skoda','citiGoBig',0,0,0,4);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Jerba','J-Pod',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Jerba','J-Pod',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Jerba','J-Pod',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Jerba','J-Pod',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Rolling homes','Vito',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Rolling homes','Vito',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Rolling homes','Vito',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Rolling homes','Vito',0,0,0,5);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Fiat','Alko Esential',0,0,0,6);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Fiat','Alko Esential',0,0,0,6);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Fiat','Alko Esential',0,0,0,6);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Concorde','Atego',0,0,0,6);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Concorde','Atego',0,0,0,6);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Globetrotter','XLi 7850',0,0,0,7);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Globetrotter','XLi 7850',0,0,0,7);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Globetrotter','XLi 7850',0,0,0,7);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Concorde','Atego Plus',0,0,0,8);
-INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, extraPrice, typeId) VALUES ('Concorde','Atego Plus',0,0,0,8);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGo',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGo',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGo',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGo',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Volvo','45Small',0,0,2);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Volvo','45Small',0,0,2);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Volvo','45Small',0,0,2);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Ford','Ka',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Ford','Ka',0,0,1);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Mercedes','VeryBig',0,0,3);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Mercedes','VeryBig',0,0,3);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Mercedes','VeryBig',0,0,3);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGoBig',0,0,4);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Skoda','citiGoBig',0,0,4);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Jerba','J-Pod',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Jerba','J-Pod',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Jerba','J-Pod',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Jerba','J-Pod',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Rolling homes','Vito',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Rolling homes','Vito',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Rolling homes','Vito',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Rolling homes','Vito',0,0,5);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Fiat','Alko Esential',0,0,6);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Fiat','Alko Esential',0,0,6);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Fiat','Alko Esential',0,0,6);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Concorde','Atego',0,0,6);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Concorde','Atego',0,0,6);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Globetrotter','XLi 7850',0,0,7);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Globetrotter','XLi 7850',0,0,7);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Globetrotter','XLi 7850',0,0,7);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Concorde','Atego Plus',0,0,8);
+INSERT INTO motorhomes (brand, model, timesUsed, kmDriven, typeId) VALUES ('Concorde','Atego Plus',0,0,8);
 
 INSERT INTO customers (cName, number) VALUES ('John Smith', '12345678');
 INSERT INTO customers (cName, number) VALUES ('Jane Doe', '90123456');
 INSERT INTO customers (cName, number) VALUES ('John Doe', '78901234');
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
